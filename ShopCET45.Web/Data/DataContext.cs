cb30582613614d76588befd4f9351ed4dc06bf14
sidney amajor <sidney-major@live.com.pt>
2020-06-04 11:56:56 +0100

    Create database and products controller

---
+++ ShopCET45.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopCET45.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCET45.Web.Data
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {            
+        }
+    }
+}
